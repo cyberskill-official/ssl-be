2c27f567d6bc5b0464702d3227edce9367755934
Stephen Cheng <zintaen@gmail.com>
2025-07-03 18:56:14 +0700

    feat: add cityId and coordinate when register

--- src/modules/authn/authn.type.ts
+++ src/modules/authn/authn.type.ts
@@ -57,7 +57,7 @@ export interface I_Input_Register_VerifyEmail {
 }
 
 // Step 3
-export interface I_Input_Register_Location extends Pick<I_Input_Location, 'countryId' | 'raw'> { }
+export interface I_Input_Register_Location extends Pick<I_Input_Location, 'countryId' | 'cityId' | 'coordinates' | 'raw'> { }
 
 export interface I_Input_Register_PersonalInfo_Partner extends Pick<I_Input_UserPartner, 'gender' | 'dateOfBirth' | 'location'> {
     location: I_Input_Register_Location;
